4ea833820062fd8514703d173c882d64937edee2
Frat Quintero <frat.quintero@gmail.com>
2022-02-25 11:28:41 -0500

    add open and rst

--- rst.sh
+++ rst.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/bash
 
-var=$(echo -e "Caracol\nVOA\nNews" | dmenu -i -p "Select Station")
+var=$(echo -e "RCN\nVOA\nNews" | dmenu -i -p "Select Station")
 
 case $var in
-	Caracol) mpv https://zeno.fm/radio/radiocaracolbogota/ ;;
+	RCN) mpv https://zeno.fm/radio/rcn-radioxybwqhc9ewzuv/ ;;
 	VOA) mpv https://www.voanews.com/live/audio/60 ;;
 	News) mpv https://zeno.fm/radio/newsradio-100/ ;;
 	*) echo "No radiostation with such a name: $var"
