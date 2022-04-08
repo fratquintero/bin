eabf97a657f449932a19604a8e24cfc0a4ce5399
Frat Quintero <frat.quintero@gmail.com>
2022-04-08 11:43:24 -0500

    adding/updating sh files

--- print_message.sh
+++ print_message.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#frat.quintero@gmail.com
 
+#Wed 16 Mar 2022
 
-until [ -z $1 ]
+echo There are $# arguments:
+for arg in $*
 do
-	echo -n $1
-	echo -n " "
-	shift
+  echo -n "$arg "
 done
+
 echo
 exit
